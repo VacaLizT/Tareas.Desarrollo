315e542be10fbe9d91264baee0e38597089d5e59
VacaLizT <vanesatzul6@gmail.com>
2025-04-22 15:30:20 -0600

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,55 +1,29 @@
-numero=int(input("Ingrese un número: "))
+n=int(input("Ingrese la cantidad de clientes: "))
+p=n
+lista=[]
+promedio=0
+uno, dos, tres, cuatro, cinco=0,0,0,0,0
 suma=0
-n=1
-while n<=numero:
-    suma=suma+n
-    n=n+1
-print("La suma de los números es",suma)
+while n!=0:
+    nota=int(input("Ingrese la calificación: "))
+    lista.append(nota)
+    suma=suma+nota
+    n=n-1
+    if nota ==1:
+        uno+=1
+    elif nota==2:
+        dos+=1
+    elif nota==3:
+        tres+=1
+    elif nota==4:
+        cuatro+=1
+    elif nota==5:
+        cinco+=1
+print(f"Malo: {uno}, Regular: {dos}, Bueno: {tres}, Muy bueno: {cuatro}, Excelente: {cinco}")
+prom=suma/p
+print(f"El promedio es: {prom}")
+for i in lista:
+    if i <prom:
+        promedio=promedio+1
+print((promedio*100/p), "% está debajo del promedio")
 
-
-
-numero=int(input("Ingrese un número: "))
-multiplicacion=1
-n=1
-while n<=numero:
-    multiplicacion=multiplicacion*n
-    n=n+1
-print("El factorial de los números es: ",multiplicacion)
-
-
-
-num=34
-variable=int(input("Adivine un número de 1 al 100. Ingrese el número que cree que es: "))
-while variable!=num:
-    if (variable<0) or ( variable>100):
-        print("Número incorrecto.")
-        variable=int(input("Intentelo de nuevo: "))
-    if (variable<0) or ( variable>100):
-        print("Número incorrecto.")
-        variable=int(input("Intentelo de nuevo: "))  
-    elif variable<num:
-        print("El número es mayor.")
-        variable=int(input("Intentelo de nuevo: "))    
-    elif variable>num:
-        print("El número es menor.")
-        variable=int(input("Intentelo de nuevo: "))
-if variable==num:
-    print("Felicidades respuesta correcta")
-    
-    
-    
-contraseña=147258369
-ingresar=int(input("Ingrese la contraseña: "))
-while ingresar!=contraseña:
-    if ingresar!=contraseña:    
-        print("Contraseña incorrecta.")
-        ingresar=int(input("Intentelo de nuevo: "))
-    elif ingresar!=contraseña:
-        print("Contraseña incorrecta.")
-        ingresar=int(input("Intentelo de nuevo: "))
-        break
-    elif ingresar!=contraseña:
-        print("Contraseña incorrecta, su cuenta sera bloqueada por los próximos 36 años. '_' .")
-        break
-if contraseña==ingresar:
-    print("Contraseña correcta. :)")
